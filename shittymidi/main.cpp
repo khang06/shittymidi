1dca8e0d80f0830d95d2433d5d3df6e2f280275d
khang06 <khang06@users.noreply.github.com>
2019-03-29 20:04:12 -0400

    note display improvements
    - cmd cursor no longer rapidly scrolls
    - no more overlap if the previous message was longer than the current
    - probably faster (it probably didn't need to be)

--- shittymidi/main.cpp
+++ shittymidi/main.cpp
@@ -34,6 +34,9 @@ int main(int argc, char** argv) {
         uint64_t last_note_check = 0;
         uint64_t notes_at_last_check = 0;
         uint64_t notes_since_last_check = 0;
+        size_t previous_string_size = 0;
+        size_t current_string_size = 0;
+        char message[0x100]; // overkill?
 
         std::cout << "Header Chunk:" << std::endl;
         std::cout << "    Length: " << midi->header_len << std::endl;
@@ -76,7 +79,17 @@ int main(int argc, char** argv) {
                 notes_at_last_check = player->played_notes;
                 last_note_check = GetTickCount64();
             }
-            std::cout << "\rPlayed " << player->played_notes << " notes | " << notes_since_last_check << " n/s" << std::flush;
+            // stringstreams are slow, let's do it c-style!
+            // any unused space will be replaced with a space to prevent overlapping if the previous message was longer than the current
+            // it only needs to be done once per above case
+            snprintf(message, 0x100, "\rPlayed %llu notes | %llu n/s", player->played_notes, notes_since_last_check);
+            current_string_size = strnlen(message, 0x100);
+            if (previous_string_size > current_string_size) {
+                memset(message + current_string_size, ' ', previous_string_size - current_string_size);
+                message[previous_string_size + 1] = '\0';
+            }
+            previous_string_size = current_string_size;
+            printf("%s", message);
         }
 
         // wait for the thread to exit
